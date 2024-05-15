c21ef14d8f359e5fbb22d4eb3ec8f7f937c3e480
rakasaputra23 <160686630+rakasaputra23@users.noreply.github.com>
2024-05-15 10:17:56 +0700

    Add files via upload

---
+++ pendaftaran_siswa/pendaftaran_siswa _baru/pendaftaran_siswa.sql
@@ -0,0 +1,62 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 30 Apr 2024 pada 07.00
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pendaftaran_siswa`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `calon_siswa`
+--
+
+CREATE TABLE `calon_siswa` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(78) NOT NULL,
+  `alamat` varchar(255) NOT NULL,
+  `jenis_kelamin` varchar(16) NOT NULL,
+  `agama` varchar(64) NOT NULL,
+  `sekolah_asal` varchar(84) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `calon_siswa`
+--
+ALTER TABLE `calon_siswa`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `calon_siswa`
+--
+ALTER TABLE `calon_siswa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
